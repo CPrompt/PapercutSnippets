18b16ca8e5b48b85898f10638c743cce94bc2fda
U-SYSTEL\cadkins <cadkins@50-1933.systel.local>
2020-10-20 13:19:57 -0400

    Added PowerShell script to output CSV file of printer names and IP's

---
+++ Powershell/printers_names_ips.ps1
@@ -0,0 +1,14 @@
+############################################################################# 
+# Exporting, Printer Name, IP Address, Driver from print server to a CSV file 
+# 
+# Rahmatullah Fedayizada 
+# CIS Assistant 
+# Mobile: 0796660969 
+# Email: rahmat_fedayizada@hotmail.com 
+# ============================================================================ 
+ 
+## Put your own print server name
+
+ 
+$printserver = "<SERVER_NAME>" 
+Get-WMIObject -class Win32_Printer -computer $printserver | Select Name,DriverName,PortName | Export-CSV -path 'C:\printers.csv'
