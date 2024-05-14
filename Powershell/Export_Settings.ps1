8cba03d928ff2cf4be62a5fefd9e247a2996a91b
U-SYSTEL\cadkins <cadkins@50-1933.systel.local>
2024-05-14 16:42:41 -0400

    Export from multiple servers

---
+++ Powershell/Export_Settings.ps1
@@ -0,0 +1,10 @@
+﻿# make the settings needed for each different manufacturer and run this.
+# it will create a template *.dat file that can then be used to import with the 
+# larger script
+
+$reference_printers = "KM Testing", "GBO Reception"
+
+foreach ($reference_printer in $reference_printers){
+    $GPC = get-printconfiguration -PrinterName $reference_printer 
+    RUNDLL32.EXE PRINTUI.DLL,PrintUIEntry /Ss /n $PNA /a C:\temp\$reference_printer.dat
+}
